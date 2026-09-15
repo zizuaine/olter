b31ccfe16237548667c05b490bbd3575e648d12b
zizuaine <Itsuki7444@gmail.com>
2026-09-15 14:17:55 +0530

    configure production cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,11 @@ import cors from "cors";
 const app = express();
 
 app.use(cors({
-    origin: "http://localhost:5173"
+    origin: [
+        "http://localhost:5173",
+        "https://olter-mrvg.vercel.app"
+    ],
+    credentials: true
 }));
 
 app.use(express.json());
