e4fe76f689a23ac1fd0631b0a8deede665b5b8d9
zizuaine <Itsuki7444@gmail.com>
2026-09-16 04:25:54 +0530

    debug

--- backend/src/config/cohere.ts
+++ backend/src/config/cohere.ts
@@ -2,10 +2,10 @@ import { CohereClientV2 } from "cohere-ai";
 import dotenv from "dotenv";
 dotenv.config();
 
-if (!process.env.Cohere_API_KEY) {
-    throw new Error("GROQ_API_KEY not provided");
+if (!process.env.COHERE_API_KEY) {
+    throw new Error("COHERE_API_KEY not provided");
 }
 
 export const cohere = new CohereClientV2({
-    token: process.env.Cohere_API_KEY
+    token: process.env.COHERE_API_KEY
 })
